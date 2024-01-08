a82ae549f306b73da55077b73cbe514729723f58
thaparup <thaparup3965@gmail.com>
2024-01-08 19:37:27 +0545

    created about section of the developer

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -81,14 +81,19 @@ export default function Intro() {
                     <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
                 </Link>
 
-                <a
-                    className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
-                    href="/CV.pdf"
-                    download
-                >
-                    Download CV{" "}
-                    <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
-                </a>
+
+                <div className="bg-white rounded-3xl">
+
+                    <a
+                        className="group px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+                        href="/CV.pdf"
+                        download
+                    >
+                        Download CV{" "}
+                        <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+                    </a>
+
+                </div>
 
                 <a
                     className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
@@ -97,6 +102,7 @@ export default function Intro() {
                 >
                     <BsLinkedin />
                 </a>
+
 
                 <a
                     className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
